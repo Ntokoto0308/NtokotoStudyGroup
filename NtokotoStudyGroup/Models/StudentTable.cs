661e3ff3d223656bd696329ffdbb6e2dccb3070f
Ntokoto Mongwe <ntokoto0308@gmail.com>
2025-03-03 11:50:32 +0200

    Add StudentTable model

---
+++ NtokotoStudyGroup/Models/StudentTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace NtokotoStudyGroup.Models
+{
+    public class StudentTable
+    {
+        [DisplayName("STUDENT NUMBER")]
+        public string Student_Number { get; set; }
+
+        [DisplayName("NAME")]
+        public string Student_Name { get; set; }
+
+        [DisplayName("SURNAME")]
+        public string Student_Surname { get; set; }
+
+        [DisplayName("EMAIL ADDRESS")]
+        public string Student_Email { get; set; }
+    }
+}
